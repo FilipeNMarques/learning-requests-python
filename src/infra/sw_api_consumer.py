cdfdb80c8b7710ac5384d320f3728d5f122488ff
Filipe Marques <filipi.marques@gmail.com>
2022-06-16 14:31:21 -0300

    wip: add get_starship_information method

--- src/infra/sw_api_consumer.py
+++ src/infra/sw_api_consumer.py
@@ -14,7 +14,11 @@ class SwApiConsumer(SwApiConsumerInterface):
 
     def __init__(self) -> None:
         self.get_starships_response = namedtuple(
-            "GET_Starships", ["status_code", "request", "response"]
+            "GET_starships",
+            ["status_code", "request", "response"],
+        )
+        self.get_starship_information_response = namedtuple(
+            "GET_starship_info", ["status_code", "request", "response"]
         )
 
     def get_starships(self, page: int) -> Tuple[int, Type[Request], Dict]:
@@ -33,6 +37,42 @@ class SwApiConsumer(SwApiConsumerInterface):
 
         if (status_code >= 200) and (status_code <= 299):
             return self.get_starships_response(
+                status_code=status_code, request=req, response=response.json()
+            )
+        else:
+            raise HttpRequestError(
+                message=response.json()["detail"], status_code=status_code
+            )
+
+    def get_starship_information(
+        self, starship_id: int
+    ) -> Tuple[int, Type[Request], Dict]:
+        """get_starship_information _summary_
+
+        _extended_summary_
+
+        Parameters
+        ----------
+        starship_id : int
+            _description_
+
+        Returns
+        -------
+        Tuple[int, Type[Request], Dict]
+            _description_
+        """
+
+        req = requests.Request(
+            method="GET",
+            url=f"https://swapi.dev/api/starships/{starship_id}",
+        )
+        req_prepared = req.prepare()
+
+        response = self.__send_http_request(req_prepared)
+        status_code = response.status_code
+
+        if (status_code >= 200) and (status_code <= 299):
+            return self.get_starship_information_response(
                 status_code=status_code, request=req, response=response.json()
             )
         else:
